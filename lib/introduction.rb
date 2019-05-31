82e7a344226b35c586aa6bbce2e06e141c53c9d2
Raza Tahir Shareef <rtshareef@gmail.com>
2019-05-31 19:31:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
@@ -5,3 +6,14 @@ end
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+=======
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
+>>>>>>> 3a1a20f458b950c985dd84bb6e3fdb54df2b8ccc
